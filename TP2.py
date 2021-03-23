241bc99d4f0d6f1e6f060d20b29edf2455ff0051
mdjamion <diak24089@gmail.com>
2021-03-23 13:51:58 +0100

    2eme modification TP2

--- TP2.py
+++ TP2.py
@@ -1,6 +1,13 @@
-def volBoite(x1, x2, x3) :
-  "Volume d'une boîte parallélipipédique"
-  return x1 * x2 * x3
+def maximum(n1, n2, n3): 
+  "Renvoie le plus grand de trois nombres" 
+  if n1 >= n2 and n1 >= n3: 
+      return n1 
+  elif n2 >= n1 and n2 >= n3: 
+      return n2 
+  else: 
+      return n3 
  
-# test :
-print(volBoite(5.2, 7.7, 3.3))
+# test : 
+print(maximum(4.5, 5.7, 3.9)) 
+print(maximum(8.2, 2.1, 6.7)) 
+print(maximum(1.3, 4.8, 7.6))
